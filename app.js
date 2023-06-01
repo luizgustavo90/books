0fad7257b6165a8c7c6244eea9d7a97b936cf3b8
Luiz Gustavo <luizdsouza.90@gmail.com>
2023-06-01 18:12:04 -0300

    add authors service and routes features

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authorsRouter = require('./routes/authors');
 var booksRouter = require('./routes/books');
 
 var app = express();
@@ -16,12 +16,12 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/authors', authorsRouter);
 app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
